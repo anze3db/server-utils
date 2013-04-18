9741fa4dd561f9a32dc6e204d7e91cbc951aa062
Smotko <ensmotko@gmail.com>
2013-04-19 00:17:46 +0200

    Added colors

--- check.py
+++ check.py
@@ -9,13 +9,13 @@ def checkWebsites():
     sites = ['http://psywerx.net', 'http://gitlab.psywerx.net', 'http://stejem.psywerx.net/',
              'http://vodim.si', 'http://slabefotke.com', 'http://slabescene.com/', 
              'http://siviljstvo-alenka.com', 'http://vezenje-kranj.com', 'http://maleficusangelus.com',
-             'http://mavricnistumfki.net']
+             'http://mavricnistumfki.net', 'http://scoreboards.psywerx.net']
 
     for s in sites:
         try:
             r = requests.get(s)
             if r.status_code != 200:
-                print s, "returned", r.status_code
+                print_red("  %s returned %s" % (s, r.status_code))
         except requests.exceptions.ConnectionError:
                 print s, "connection error"
 
@@ -35,9 +35,10 @@ def checkServices():
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = s.connect_ex((address, port))
         if(result != 0):
-            print name, "seems to be down"    
+            print_red("  %s seems to be down" % name)    
     
-    
+def print_red(s):
+    print '\033[91m%s\033[0m' % s    
 
 if __name__ == "__main__":
     checkWebsites()
